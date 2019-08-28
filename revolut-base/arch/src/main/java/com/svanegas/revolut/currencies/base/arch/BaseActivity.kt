8377620961fb9b40cd85d8d9178e16655847720a
Santiago Vanegas Gil <savanegasg@gmail.com>
2019-08-27 22:17:44 -0700

    Add arch module with starting base activity and fragment

---
+++ revolut-base/arch/src/main/java/com/svanegas/revolut/currencies/base/arch/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.svanegas.revolut.currencies.base.arch
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity()
---
+++ revolut-base/arch/src/main/java/com/svanegas/revolut/currencies/base/arch/BaseFragment.kt
@@ -0,0 +1,5 @@
+package com.svanegas.revolut.currencies.base.arch
+
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment()
