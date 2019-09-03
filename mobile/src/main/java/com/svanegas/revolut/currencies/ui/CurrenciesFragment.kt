a8b9078ba45b7365833d30c3fd9c2e743e61cc85
Santiago Vanegas Gil <savanegasg@gmail.com>
2019-09-02 19:30:28 -0700

    Use styles for toolbar and general elements

--- mobile/src/main/java/com/svanegas/revolut/currencies/ui/CurrenciesFragment.kt
+++ mobile/src/main/java/com/svanegas/revolut/currencies/ui/CurrenciesFragment.kt
@@ -3,8 +3,11 @@ package com.svanegas.revolut.currencies.ui
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.svanegas.revolut.currencies.R
 import com.svanegas.revolut.currencies.base.arch.BaseFragmentViewModel
 import com.svanegas.revolut.currencies.base.arch.BaseView
 import com.svanegas.revolut.currencies.databinding.FragmentCurrenciesBinding
@@ -34,8 +37,15 @@ class CurrenciesFragment : BaseFragmentViewModel<CurrenciesViewModel, FragmentCu
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupToolbar()
         binding.currenciesRecycler.adapter = currenciesAdapter
         binding.currenciesRecycler.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun setupToolbar() {
+        val toolbar: Toolbar = binding.toolbarLayout.findViewById(R.id.toolbar)
+        toolbar.title = getString(R.string.currencies_list_title)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
